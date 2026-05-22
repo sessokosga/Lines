58c5db4fad72158b0d7b26a9f7f427881482c148
Michée Kosga <micheekosga@gmail.com>
2026-05-22 08:11:27 +0100

    added 12 levels, 3 for each grid size

--- assets/scripts/LevelData.ts
+++ assets/scripts/LevelData.ts
@@ -11,48 +11,167 @@ export interface LevelData {
 }
 
 export const LEVELS: LevelData[] = [
+    // --- SIZE 4x4 (Levels 1-3) ---
     {
-        id: 1,
-        gridSize: 4,
+        id: 1, gridSize: 4,
         nodes: [
             { number: 1, row: 0, col: 0 },
             { number: 2, row: 3, col: 0 },
-            { number: 3, row: 3, col: 3 },
-            { number: 4, row: 0, col: 3 }
+            { number: 3, row: 3, col: 3 }
         ]
     },
     {
-        id: 2,
-        gridSize: 5,
+        id: 2, gridSize: 4,
+        nodes: [
+            { number: 1, row: 0, col: 1 },
+            { number: 2, row: 2, col: 0 },
+            { number: 3, row: 3, col: 2 },
+            { number: 4, row: 1, col: 3 }
+        ]
+    },
+    {
+        id: 3, gridSize: 4,
+        nodes: [
+            { number: 1, row: 1, col: 1 },
+            { number: 2, row: 0, col: 3 },
+            { number: 3, row: 3, col: 3 },
+            { number: 4, row: 3, col: 0 },
+            { number: 5, row: 0, col: 0 }
+        ]
+    },
+
+    // --- SIZE 5x5 (Levels 4-6) ---
+    {
+        id: 4, gridSize: 5,
         nodes: [
             { number: 1, row: 0, col: 0 },
-            { number: 2, row: 2, col: 2 },
-            { number: 3, row: 4, col: 4 }
+            { number: 2, row: 0, col: 4 },
+            { number: 3, row: 4, col: 4 },
+            { number: 4, row: 4, col: 0 },
+            { number: 5, row: 2, col: 2 },
+            { number: 6, row: 1, col: 1 }
         ]
     },
     {
-        id: 3,
-        gridSize: 6,
+        id: 5, gridSize: 5,
+        nodes: [
+            { number: 1, row: 4, col: 0 },
+            { number: 2, row: 0, col: 0 },
+            { number: 3, row: 0, col: 4 },
+            { number: 4, row: 2, col: 4 },
+            { number: 5, row: 2, col: 0 },
+            { number: 6, row: 4, col: 2 },
+            { number: 7, row: 1, col: 2 }
+        ]
+    },
+    {
+        id: 6, gridSize: 5,
+        nodes: [
+            { number: 1, row: 2, col: 0 },
+            { number: 2, row: 0, col: 0 },
+            { number: 3, row: 0, col: 4 },
+            { number: 4, row: 4, col: 4 },
+            { number: 5, row: 4, col: 0 },
+            { number: 6, row: 2, col: 4 },
+            { number: 7, row: 2, col: 2 },
+            { number: 8, row: 1, col: 1 }
+        ]
+    },
+
+    // --- SIZE 6x6 (Levels 7-9) ---
+    {
+        id: 7, gridSize: 6,
         nodes: [
             { number: 1, row: 0, col: 0 },
             { number: 2, row: 5, col: 0 },
             { number: 3, row: 5, col: 5 },
-            { number: 4, row: 0, col: 5 }
+            { number: 4, row: 0, col: 5 },
+            { number: 5, row: 1, col: 1 },
+            { number: 6, row: 4, col: 4 },
+            { number: 7, row: 2, col: 2 },
+            { number: 8, row: 3, col: 3 },
+            { number: 9, row: 0, col: 2 }
         ]
     },
     {
-        id: 4,
-        gridSize: 7,
+        id: 8, gridSize: 6,
         nodes: [
-            { "number": 1, "row": 3, "col": 3 },
-            { "number": 2, "row": 1, "col": 1 },
-            { "number": 3, "row": 5, "col": 5 },
-            { "number": 4, "row": 6, "col": 0 },
-            { "number": 5, "row": 6, "col": 6 },
-            { "number": 6, "row": 0, "col": 6 },
-            { "number": 7, "row": 0, "col": 0 },
-            { "number": 8, "row": 4, "col": 4 },
-            { "number": 9, "row": 2, "col": 2 }
+            { number: 1, row: 3, col: 0 },
+            { number: 2, row: 0, col: 0 },
+            { number: 3, row: 0, col: 5 },
+            { number: 4, row: 5, col: 5 },
+            { number: 5, row: 5, col: 0 },
+            { number: 6, row: 2, col: 1 },
+            { number: 7, row: 1, col: 4 },
+            { number: 8, row: 4, col: 3 },
+            { number: 9, row: 2, col: 4 },
+            { number: 10, row: 0, col: 3 }
+        ]
+    },
+    {
+        id: 9, gridSize: 6,
+        nodes: [
+            { number: 1, row: 5, col: 2 },
+            { number: 2, row: 3, col: 0 },
+            { number: 3, row: 0, col: 0 },
+            { number: 4, row: 0, col: 5 },
+            { number: 5, row: 5, col: 5 },
+            { number: 6, row: 5, col: 0 },
+            { number: 7, row: 3, col: 3 },
+            { number: 8, row: 1, col: 1 },
+            { number: 9, row: 4, col: 4 },
+            { number: 10, row: 2, col: 2 },
+            { number: 11, row: 0, col: 3 }
+        ]
+    },
+
+    // --- SIZE 7x7 (Levels 10-12) ---
+    {
+        id: 10, gridSize: 7,
+        nodes: [
+            { number: 1, row: 3, col: 3 },
+            { number: 2, row: 1, col: 1 },
+            { number: 3, row: 5, col: 5 },
+            { number: 4, row: 6, col: 0 },
+            { number: 5, row: 6, col: 6 },
+            { number: 6, row: 0, col: 6 },
+            { number: 7, row: 0, col: 0 },
+            { number: 8, row: 4, col: 4 },
+            { number: 9, row: 2, col: 2 },
+            { number: 10, row: 0, col: 4 }
+        ]
+    },
+    {
+        id: 11, gridSize: 7,
+        nodes: [
+            { number: 1, row: 0, col: 0 },
+            { number: 2, row: 0, col: 6 },
+            { number: 3, row: 6, col: 6 },
+            { number: 4, row: 6, col: 0 },
+            { number: 5, row: 3, col: 0 },
+            { number: 6, row: 3, col: 6 },
+            { number: 7, row: 1, col: 3 },
+            { number: 8, row: 5, col: 3 },
+            { number: 9, row: 2, col: 1 },
+            { number: 10, row: 4, col: 5 },
+            { number: 11, row: 0, col: 3 }
+        ]
+    },
+    {
+        id: 12, gridSize: 7,
+        nodes: [
+            { number: 1, row: 6, col: 3 },
+            { number: 2, row: 3, col: 0 },
+            { number: 3, row: 0, col: 0 },
+            { number: 4, row: 0, col: 6 },
+            { number: 5, row: 6, col: 6 },
+            { number: 6, row: 6, col: 0 },
+            { number: 7, row: 4, col: 1 },
+            { number: 8, row: 1, col: 2 },
+            { number: 9, row: 2, col: 5 },
+            { number: 10, row: 5, col: 4 },
+            { number: 11, row: 3, col: 3 },
+            { number: 12, row: 0, col: 3 }
         ]
     }
 ];
